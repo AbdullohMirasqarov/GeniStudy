7ff9349af95d88e0eec0decfa05b887a25974328
Abdulloh9090 <muske4030@gmail.com>
2025-12-19 15:24:34 +0500

    v0.11

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:5533"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:5533", "https://geni-study.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
